e41360e03e75def5a20dae710f947d2ae7af566a
akshay060805 <vanama.akshay06@gmail.com>
2026-01-31 17:36:08 +0530

    cors update

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 app.use(cors({ 
-    origin: "http://localhost:5173", 
+    origin: ["http://localhost:5173","https://revelation2k26-frontend-one.vercel.app"], 
     // origin: ["https://revelation2k25-frontend-testing.vercel.app", "http://localhost:5173", "https://revelation2k25.vercel.app"],
     credentials: true 
 }));
